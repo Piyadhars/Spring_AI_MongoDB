eaa5d25f162e6161e2b32ee8da4a48b02bba812f
Priyadharshini M <m.priyadharshinimanak@gmail.com>
2022-09-23 11:13:21 +0530

    Sping API CRUD operations using Mongodb

---
+++ src/main/java/com/example/spring_mongo/APIMongoController.java
@@ -0,0 +1,94 @@
+package com.example.spring_mongo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class APIMongoController {
+    @Autowired
+    EmployeeRepository repo;
+
+    @PostMapping("/api/employee")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee emp) {
+        if (emp.id != null && emp.name != null && emp.getAge()!= null) {
+            List<Employee> result = new ArrayList<>();
+            result = repo.findAll();
+            List<String> idlist = new ArrayList<>();
+            for (Employee e : result) {
+                idlist.add(e.id);
+            }
+            if (idlist.contains(emp.id)) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Id is already exists");
+            } else {
+                repo.save(new Employee(emp.id, emp.name, emp.age));
+                return ResponseEntity.status(HttpStatus.ACCEPTED).body("New Employee is added");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Enter all details");
+        }
+
+    }
+
+    @GetMapping("/api/employee")
+    public ResponseEntity<List<Employee>> showdetail() {
+        return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
+    }
+
+    @PutMapping("/api/employee/{id}")
+    public ResponseEntity<String> updatedetail(@PathVariable String id, @RequestBody Employee emp) {
+        List<Employee> details = new ArrayList<>();
+        int a=0;
+        int b=0;
+        details = repo.findAll();
+        List<String> idlist = new ArrayList<>();
+        for (Employee e : details) {
+            idlist.add(e.id);
+        }
+        if (idlist.contains(id)) {
+            Optional<Employee> result = repo.findById(id);
+            String tell=result.get().age;
+            if((emp.age != null ) && (!tell.equals(emp.age))&&(emp.name != null)&&(!result.get().name.equals(emp.name))) {
+                result.get().age = emp.age;
+                result.get().name=emp.name;
+                repo.save(result.get());
+                return ResponseEntity.status(HttpStatus.OK).body("Updated");
+            }
+            if((emp.age != null ) && (!tell.equals(emp.age))) {
+                result.get().age = emp.age;
+                repo.save(result.get());
+                a=1;
+                return ResponseEntity.status(HttpStatus.OK).body("Updated the age");
+            }
+            if ((emp.name != null)&&(!result.get().name.equals(emp.name))){
+            result.get().name = emp.name;
+            repo.save(result.get());
+            b=1;
+            return ResponseEntity.status(HttpStatus.OK).body("Updated the name");
+            }
+            else{
+                return ResponseEntity.status(HttpStatus.ACCEPTED).body("Already up to date");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Id Not Found");
+    }
+    @DeleteMapping("/api/employee")
+    public ResponseEntity<String> del(@RequestBody Employee emp){
+        List<Employee> result = new ArrayList<>();
+        result = repo.findAll();
+        List<String> idlist = new ArrayList<>();
+        for (Employee e : result) {
+            idlist.add(e.id);
+        }
+        if (idlist.contains(emp.id)){
+            repo.deleteById(emp.id);
+           return ResponseEntity.status(HttpStatus.ACCEPTED).body("Deleted the employee details");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Does not exist");
+    }
+}
---
+++ src/main/java/com/example/spring_mongo/Employee.java
@@ -0,0 +1,56 @@
+package com.example.spring_mongo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document
+public class Employee {
+    @Id
+    public String id;
+
+    @Field
+   public String name;
+
+    @Field
+    public String age;
+
+   public Employee(){}
+    public Employee(String id,String name,String age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
